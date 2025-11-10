ecf39c98b0bdf740aaff7652be98c7cbc25ab7ab
Oscar <78726017+osyyyS@users.noreply.github.com>
2025-11-10 11:41:18 +0100

    Remove WinForms form from tray app

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ClipInjector
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ClipInjectorTrayForm());
+            Application.Run(new ClipInjectorTrayContext());
         }
     }
 }
