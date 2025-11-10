d1172bbf9a52529abf899cf563dfaeda42832250
Oscar <78726017+osyyyS@users.noreply.github.com>
2025-11-10 11:31:32 +0100

    🧹cleanup

--- ClipInjectorTrayForm.cs
+++ ClipInjectorTrayForm.cs
@@ -1,8 +1,8 @@
 namespace ClipInjector
 {
-    public partial class Form1 : Form
+    public partial class ClipInjectorTrayForm : Form
     {
-        public Form1()
+        public ClipInjectorTrayForm()
         {
             InitializeComponent();
         }
